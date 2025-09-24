5d5511f8ccba6f5960b5c68e5c33acf42c9cbc27
Alej0o8 <alejandro.uribe@uniminuto.edu.co>
2025-09-24 17:06:43 -0500

    Add files via upload

--- PythonProject/automatizar.bat
+++ PythonProject/automatizar.bat
@@ -1,5 +1,32 @@
 @echo off
 cls
 
-:: obtener el nombre del archivo sin extension
-s
+:: Obtener el nombre del archivo sin extensión
+set "projectName=%~n0"
+
+:: Crear carpeta con el nombre del archivo
+mkdir "%projectName%"
+cd "%projectName%"
+
+:: Crear subcarpetas
+mkdir notebooks src
+
+:: Crear entorno virtual
+py -m venv .venv
+
+:: Crear estructura MVC dentro de src
+cd src
+mkdir model view controller
+
+:: Activar entorno virtual
+cd..
+cd .venv\Scripts
+call activate
+
+:: Actualizar python
+cd..
+cd..
+py -m pip install --upgrade pip
+
+::Entrar entorno de programacion vsc
+code .
